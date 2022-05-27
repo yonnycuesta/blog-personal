b502b418602a5886578b146c919a9ec4f908dc21
Yonny Cuesta <yonny.cuesta2017@unaula.edu.co>
2022-05-27 01:15:42 -0500

    Details Upload Serve

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Brian2694\Toastr\ToastrServiceProvider::class,
 
 
 
@@ -195,7 +194,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Toastr' => Brian2694\Toastr\Facades\Toastr::class,
 
     ])->toArray(),
 
